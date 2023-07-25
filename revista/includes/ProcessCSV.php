e5193c10c5ee00e6d4c43b0cd20d10c861bfdd10
Marcelo Vieira <marceloquinze@gmail.com>
2023-07-25 12:00:01 -0300

    Some comments added

--- revista/includes/ProcessCSV.php
+++ revista/includes/ProcessCSV.php
@@ -6,34 +6,37 @@ class ProcessCSV
     private $urls = array();
     private $table;
 
-    public function __construct(){
+    public function __construct()
+    {
         // 'csv_file' é o name do input file
-        if( isset( $_FILES['csv_file']) && $_FILES['csv_file']['error'] == 0 ){
+        if (isset($_FILES['csv_file']) && $_FILES['csv_file']['error'] == 0) {
             $csv_file = $_FILES['csv_file']['tmp_name'];
             // envia o arquivo para o método processFile
             // este arquivo vai ficar sendo passado de método em método por parâmetro
-            $result = $this->processFile( $csv_file );
+            $result = $this->processFile($csv_file);
             return $result;
         }
     }
 
     // Este método faz o meio de campo entre o upload e o processamento do arquivo
-    private function processFile( $csv_file ){
-        $result = $this->get_csv( $csv_file );
-        $this->create_images( $csv_file );
+    private function processFile($csv_file)
+    {
+        $result = $this->get_csv($csv_file);
+        $this->create_images($csv_file);
         return $result;
     }
 
-    private function get_csv( $csv_file ){
-        $file_to_read = fopen( $csv_file, 'r' );
-        while ( !feof( $file_to_read ) ) {
-            $lines[] = fgetcsv( $file_to_read, 0, ',' );
+    private function get_csv($csv_file)
+    {
+        $file_to_read = fopen($csv_file, 'r');
+        while (!feof($file_to_read)) {
+            $lines[] = fgetcsv($file_to_read, 0, ',');
         }
-        fclose( $file_to_read );
+        fclose($file_to_read);
 
         $result = array();
 
-        foreach( $lines as $line ){
+        foreach ($lines as $line) {
             $result[] = [
                 'id'    => $line[0],
                 'url'   => $line[1]
@@ -43,26 +46,27 @@ class ProcessCSV
         return $result;
     }
 
-    private function create_images( $csv_file ){
-        $source = $this->get_csv( $csv_file );
+    private function create_images($csv_file)
+    {
+        $source = $this->get_csv($csv_file);
 
-        for ( $i=0; $i < count($source); $i++) { 
-            $this->ids[] = (int) $source[$i]['id']; 
-            $this->urls[] = $source[$i]['url']; 
+        for ($i = 0; $i < count($source); $i++) {
+            $this->ids[] = (int) $source[$i]['id'];
+            $this->urls[] = $source[$i]['url'];
         }
 
         $path = wp_upload_dir();
         $imgpath = $path['path'];
         $image_url = $path['url'];
-        $image_subdir = ltrim( $path['subdir'], '/' );
+        $image_subdir = ltrim($path['subdir'], '/');
 
-        foreach( $this->urls as $i => $url ){
-            $content = file_get_contents( $url );
-            wp_upload_bits( 'post-' . $this->ids[$i] . '.jpg', null, $content );
+        foreach ($this->urls as $i => $url) {
+            $content = file_get_contents($url);
+            wp_upload_bits('post-' . $this->ids[$i] . '.jpg', null, $content);
 
-            $post_title = get_post_field( 'post_title', $this->ids[$i] );
+            $post_title = get_post_field('post_title', $this->ids[$i]);
 
-            $attachment_id = wp_insert_attachment( array(
+            $attachment_id = wp_insert_attachment(array(
                 'post_title' => $post_title,
                 'post_content' => '',
                 'post_status' => 'inherit',
@@ -72,21 +76,22 @@ class ProcessCSV
                 'guid' => $image_url . '/post-' . $this->ids[$i] . '.jpg'
             ));
 
-            if ( ! is_wp_error( $attachment_id ) ) {
-                $attach_data = wp_generate_attachment_metadata( $attachment_id, $imgpath . '/post-' . $this->ids[$i] . '.jpg' );
-                wp_update_attachment_metadata( $attachment_id, $attach_data );
-                update_post_meta( $attachment_id, '_wp_attached_file', $image_subdir . '/post-' . $this->ids[$i] . '.jpg');
-                update_post_meta( $this->ids[$i], '_thumbnail_id', $attachment_id);
+            // This is still printing some warnings on the screen. I'll need to investigate this further.
+            if (!is_wp_error($attachment_id)) {
+                $attach_data = wp_generate_attachment_metadata($attachment_id, $imgpath . '/post-' . $this->ids[$i] . '.jpg');
+                wp_update_attachment_metadata($attachment_id, $attach_data);
+                update_post_meta($attachment_id, '_wp_attached_file', $image_subdir . '/post-' . $this->ids[$i] . '.jpg');
+                update_post_meta($this->ids[$i], '_thumbnail_id', $attachment_id);
+
+                // Why is this not working? At the moment I need to do break the process down into the steps above.
                 //set_post_thumbnail( absint($this->ids[$i]), absint($attachment_id) );
             }
 
-            // Insere os metadados no banco de dados
+            // EXPERIMENTAL: Insere os metadados no banco de dados
             $this->table = new RevistaTable();
-            $this->table->insert( $this->ids[$i], $attachment_id, 'NULL' );
+            $this->table->insert($this->ids[$i], $attachment_id, 'NULL');
 
+            // TODO: create a method to log the process
         }
-
-        
     }
-
 }
--- revista/revista.php
+++ revista/revista.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Revista
  * Plugin URI: https://www.wordpress.org/mv-slider
- * Description: My plugin's description
+ * Description: This plugins automatically creates featured images to a specific client website
  * Version: 1.0
  * Requires at least: 5.6
  * Author: Marcelo Vieira
@@ -14,17 +14,19 @@
  * Domain Path: /languages
  */
 
-if( ! defined( 'ABSPATH') ){
+if (!defined('ABSPATH')) {
     exit;
 }
 
-if( ! class_exists( 'Revista' ) ){
-    class Revista{
+if (!class_exists('Revista')) {
+    class Revista
+    {
 
         private static $instance;
 
-        public static function get_instance(){
-            if( null === self::$instance ){
+        public static function get_instance()
+        {
+            if (null === self::$instance) {
                 self::$instance = new self();
             }
             return self::$instance;
@@ -36,52 +38,56 @@ if( ! class_exists( 'Revista' ) ){
             $this->define_constants();
 
             // Chama o arquivo que processa o CSV 
-            require_once( REVISTA_INCLUDES . '/ProcessCSV.php' );
+            require_once(REVISTA_INCLUDES . '/ProcessCSV.php');
 
             // // Chama o método que registra a tabela de metadados no banco de dados
-            require_once( REVISTA_INCLUDES . '/RevistaTable.php' );
-            
+            require_once(REVISTA_INCLUDES . '/RevistaTable.php');
+
             // Chama o método que adiciona o menu
-            add_action( 'admin_menu', array( $this, 'add_menu_page' ) );
+            add_action('admin_menu', array($this, 'add_menu_page'));
         }
-        
-        public function define_constants(){
-            define( 'REVISTA_VERSION', '1.0' );
-            define( 'REVISTA_FILE', __FILE__ );
-            define( 'REVISTA_PATH', dirname( REVISTA_FILE ) );
-            define( 'REVISTA_INCLUDES', REVISTA_PATH . '/includes' );
-            define( 'REVISTA_URL', plugins_url( '', REVISTA_FILE ) );
-            define( 'REVISTA_ASSETS', REVISTA_URL . '/assets' );
+
+        public function define_constants()
+        {
+            define('REVISTA_VERSION', '1.0');
+            define('REVISTA_FILE', __FILE__);
+            define('REVISTA_PATH', dirname(REVISTA_FILE));
+            define('REVISTA_INCLUDES', REVISTA_PATH . '/includes');
+            define('REVISTA_URL', plugins_url('', REVISTA_FILE));
+            define('REVISTA_ASSETS', REVISTA_URL . '/assets');
         }
-        
-        public function add_menu_page(){
+
+        public function add_menu_page()
+        {
             add_menu_page(
                 'Revista',
                 'Revista',
                 'manage_options',
                 'revista',
-                array( $this, 'revista_page' ),
+                array($this, 'revista_page'),
                 'dashicons-format-image',
                 20
             );
         }
-        
-        public function revista_page(){ 
+
+        public function revista_page()
+        {
             // Chama a view com o formulário de upload
             // Chama o método que processa o arquivo         
             new ProcessCSV();
-            require_once( REVISTA_PATH . '/views/revista-view.php' );
+            require_once(REVISTA_PATH . '/views/revista-view.php');
         }
 
-        public static function activate(){
+        public static function activate()
+        {
             // Chama o método que registra a tabela de metadados no banco de dados
             new RevistaTable();
         }
     }
 }
-if( class_exists( 'Revista' ) ){
+if (class_exists('Revista')) {
 
-    register_activation_hook( __FILE__, array( 'Revista', 'activate' ) );
+    register_activation_hook(__FILE__, array('Revista', 'activate'));
 
     $revista = Revista::get_instance();
 }
